a7eeb0fc4fb356ba39c28f5ec61f9e17938c6732
redslugah <rafael.douglas.1@gmail.com>
2024-02-09 16:44:12 -0300

    collision and anteroid split

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ WIN_WIDTH, WIN_HEIGHT = 600, 600
 BULLET  = pygame.image.load(os.path.join("src", "bullet.png"))
 SHIPS =[pygame.image.load(os.path.join("src", "ship.png")), pygame.image.load(os.path.join("src", "ship2.png")), pygame.image.load(os.path.join("src", "ship3.png"))]
 ASTEROID_B = pygame.image.load(os.path.join("src", "b.png"))
-ASTEROID_M = pygame.transform.scale2x(pygame.image.load(os.path.join("src", "m.png")))
-ASTEROID_S = pygame.transform.scale2x(pygame.image.load(os.path.join("src", "s.png")))
+ASTEROID_M = pygame.image.load(os.path.join("src", "m.png"))
+ASTEROID_S = pygame.image.load(os.path.join("src", "s.png"))
 BG = pygame.transform.scale2x(pygame.image.load(os.path.join("src", "bg.png")))
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 
 class bullet:
-    def __init__(self) -> None:
-        self.velocity = pygame.Vector2(0,0)
-        self.position = pygame.Vector2(200,200)
+    def __init__(self, position, acceleration,x,y):
+        self.velocity = pygame.Vector2(x,y)
+        self.position = pygame.Vector2(position.x+22, position.y+24)
+        self.acceleration = acceleration
         self.img = BULLET
 
     def move(self):
-        pass
+        self.velocity += self.acceleration
+        self.position += self.velocity
 
     def draw(self, win):
         win.blit(self.img, self.position)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
 
 
 
@@ -82,19 +87,43 @@ class ship:
         rotated_image = pygame.transform.rotate(self.img, self.tilt)
         new_rect = rotated_image.get_rect(center = self.img.get_rect(topleft = (self.position)).center)
         win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+    
                  
 class asteroid:
-    def __init__(self, size):
+    def __init__(self, size, x, y):
         if size == "B":
             self.x = random.randint(0, WIN_WIDTH)
             self.y = random.randint(0, WIN_HEIGHT)
-            self.xa = random.randint(-5, 5)
-            self.ya = random.randint(-5, 5)
+            self.xa = random.randint(-4, 4)
+            self.ya = random.randint(-4, 4)
             self.type = "B"
-            self.tilt = 0
+            self.tilt = random.randint(0,359)
             self.img = ASTEROID_B
             self.width = 133
             self.height = 160
+        if size == "M":
+            self.x = x
+            self.y = y
+            self.xa = random.randint(-7, 7)
+            self.ya = random.randint(-7, 7)
+            self.type = "M"
+            self.tilt = random.randint(0,359)
+            self.img = ASTEROID_M
+            self.width = 80
+            self.height = 80
+        if size == "S":
+            self.x = x
+            self.y = y
+            self.xa = random.randint(-10, 10)
+            self.ya = random.randint(-10, 10)
+            self.type = "S"
+            self.tilt = random.randint(0,359)
+            self.img = ASTEROID_S
+            self.width = 40
+            self.height = 40
 
     def move(self):
         self.y += self.ya
@@ -110,19 +139,39 @@ class asteroid:
             self.y = WIN_HEIGHT
 
     def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
+        aste = pygame.transform.rotate(self.img, self.tilt)
+        win.blit(aste, (self.x, self.y))
+    
+    def collide(self, bullet = None, nave = None):
+
+        if nave == None:
+            asteroid_mask = pygame.mask.from_surface(self.img)
+            bullet_mask = bullet.get_mask()
+            ba_offset = (self.x - bullet.position.x, self.y - round(bullet.position.y))
+            ba_point = bullet_mask.overlap(asteroid_mask, ba_offset)
+            if ba_point:return True
+
+        else:
+            asteroid_mask = pygame.mask.from_surface(self.img)
+            nave_mask = nave.get_mask()
+            na_offset = (self.x - nave.position.x, self.y - round(nave.position.y))
+            na_point = nave_mask.overlap(asteroid_mask, na_offset)
+            if na_point: return True
+        
+        return False    
 
 
-def draw_window(win, rock, nave, bull = None):
+def draw_window(win, rock, nave, bull = None, fly = False):
     win.blit(BG, (0,0))
-    nave.draw(win)
     try:
-        bull.draw(win)
+        for b in bull:
+            b.draw(win)
     except:
         pass
 
     for asteroids in rock:
         asteroids.draw(win)
+    nave.draw(win, fly)
 
 
     pygame.display.update()
@@ -133,12 +182,20 @@ def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
     score = 0
-    rock = [asteroid("B"), asteroid("B"), asteroid("B"), asteroid("B"), asteroid("B")]
+    #rock = [asteroid("B"), asteroid("B"), asteroid("B"), asteroid("B"), asteroid("B")]
+    rock = []
     nave = ship()
     shot = False
+    bull =[]
+    gigidy = 0
+    for ast in range(5):
+        rock.append(asteroid("B", 0, 0))
 
     while run:
-        shot = False
+        if gigidy > 30:
+            gigidy = 30
+        if gigidy < 30:
+            gigidy += 1
         clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -161,9 +218,26 @@ def main():
             nave.draw(win, True)
         if key_pressed[pygame.K_DOWN]:
             nave.velocity -= nave.acceleration
-        if key_pressed[pygame.K_SPACE]:
-            bull = bullet()
+        if key_pressed[pygame.K_SPACE] and gigidy == 30:
+            bull.append(bullet(nave.position, nave.acceleration, nave.velocity.x, nave.velocity.y))
             shot = True
+            gigidy = 0
+
+        for y,aste in enumerate(rock):
+            if aste.collide(False, nave):
+                print("ded")
+            for x, b in enumerate(bull):
+                if aste.collide(b):
+                    print("aaaaaaaaaa")
+                    bull.pop(x)
+                    if aste.type == "B":
+                        rock.append(asteroid("M", aste.x, aste.y))
+                        rock.append(asteroid("M", aste.x, aste.y))
+                    elif aste.type == "M":
+                        rock.append(asteroid("S", aste.x, aste.y))
+                        rock.append(asteroid("S", aste.x, aste.y))
+                    rock.pop(y)
+
 
         for asteroids in rock:
             asteroids.move()
@@ -173,6 +247,10 @@ def main():
             nave.velocity -=pygame.Vector2(nave.velocity/20, nave.velocity/20)
         nave.move()
         if shot:
+            for x, b in enumerate(bull):
+                if (b.position.x < 0 or b.position.x > WIN_WIDTH)or(b.position.y < 0 or b.position.y > WIN_HEIGHT):
+                    bull.pop(x)
+                b.move()
             draw_window(win, rock, nave, bull)
         else:
             draw_window(win, rock, nave)
